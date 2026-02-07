7c29bd44e3cbfceb4b5df6e18472258e2343824b
jbolivar2 <jbolivara@unal.edu.co>
2026-02-07 10:40:00 -0500

    Create ejercicio 4.py

---
+++ ejercicio 4.py
@@ -0,0 +1,10 @@
+edad_juan = float(input("Ingrese la edad de Juan: "))
+
+edad_alberto = (2 / 3) * edad_juan
+edad_ana = (4 / 3) * edad_juan
+edad_mama = edad_juan + edad_alberto + edad_ana
+
+print("Edad de Juan:", edad_juan)
+print("Edad de Alberto:", edad_alberto)
+print("Edad de Ana:", edad_ana)
+print("Edad de la mamá:", edad_mama)
